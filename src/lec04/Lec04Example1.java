a63be8fa69668d23a77d44eb6a3b58cb89127eef
Ketan Mayer-Patel <kmp@cs.unc.edu>
2019-08-28 22:26:40 -0400

    Init commit.

--- src/lec04/Lec04Example1.java
+++ src/lec04/Lec04Example1.java
@@ -4,29 +4,77 @@ public class Lec04Example1 {
 
 	public static void main(String[] args) {
 		
-		// Create a string from a literal
-		String s1 = "This is a string";
+		int[] a1;
 		
-		// Create a string using "new"
-		// String s2 = "Another string";
-		String s2 = new String("Another string");
+		// iarray is declared to be a 
+		// variable that can reference an
+		// array of int's, but the array hasn't 
+		// been created yet.
+		
+		// At this point, the value of a1 is null
+		
+		a1 = new int[10];
+		
+		// Now iarray references (aka "points to")
+		// a new array of 10 integers
+		// The default value for each element is 0.
+		
+		System.out.println("The length of iarray is: " + 
+							a1.length);
+		
+		// Indexing is done with [] as in.
+		// This is both for retrieving and setting.
+		
+		a1[0] = 42;
+		a1[1] = 2;
+		a1[2] = 120;
+		a1[3] = 5;
+		
+		// What is the value of iarray[4]?
+		// Since it hasn't been set yet, it gets the
+		// default value for the type. In this case 0.
+		
+		System.out.println("iarray[4] = " + a1[4]);
+		
+		// We can also create an array literally by
+		// specifying a sequence of values after the new
+		// constructor for the appropriate type.
 
-		// From the result of the + operator
+		a1 = new int[] {5, 6, 7};
 		
-		String s3 = "This" + " plus " + "that";
+		// Notice that I reused the a1 variable.
+		// Same variable, but now a different array.
+		// The old array will be "garbage collected"
+		// if there isn't some other reference to it.
 		
-		// Value data types automatically converted
-		// to strings if concatenated to a string using +
+		System.out.println("The length of iarray is: " +
+						   a1.length);
 		
-		int a = 4;
-		double b = 3.24;
-		String s4 = "a = " + a + ", b = " + b;
+		// If you are declaring and initializing
+		// an array variable with literals at the
+		// same time, there is a shorthand for that as in:
 		
-		System.out.println(s1);
-		System.out.println(s2);
-		System.out.println(s3);
-		System.out.println(s4);
+		String[] sa1 = {"One fish", 
+				        "Two fish", 
+				        "Red fish", 
+				        "Blue fish"};
+		
+		// A common operation is to run a for
+		// loop across the elements of an array.
+		// Here is one commonly used way to do that:
+		
+		int sum_of_lengths = 0;
+		for (int i=0; i<sa1.length; i++) {
+			String s = sa1[i];
+			sum_of_lengths += s.length();
+			
+			// Can do the above without using 
+			// an extra variable just as easily:
+			//sum_of_lengths += sa1[i].length();
+		}
+		
+		System.out.println("The sum of string lengths is: " +
+		                   sum_of_lengths);
 		
 	}
-
 }
--- src/lec04/Lec04Example2.java
+++ src/lec04/Lec04Example2.java
@@ -1,40 +1,99 @@
 package lec04;
 
 public class Lec04Example2 {
-
 	public static void main(String[] args) {
 		
-		String s = "This is my string";
+		int[] a = {1, 1, 2, 3, 5, 8};
 		
-		// Length of string
-		System.out.println("Length of s: " + s.length());
+		// We can pass arrays as arguments to another method
+		print_array("Original a", a);
 		
-		// Character access
-		char c = s.charAt(5);
-		System.out.println("The sixth character of s: " + c);
+		// Arrays are reference types, so copying an
+		// array reference from one variable to another
+		// does NOT create a new array.
+		// Instead, the two variables simply refer
+		// to the same array.
 		
-		// Testing for equality
-		if (s.equals("This is my string!")) {
-			System.out.println("Equality test is true.");
-		} else {
-			System.out.println("Equality test is false.");			
-		}
+		int[] b = a;
 		
-		// Extracting a substring
+		// This means that if we change the contents
+		// of the array using one of the variables,
+		// the contents change with respect to the other variable
+		// as well since they are simply pointing to the same array.
 		
-		String sub = s.substring(3, 12);
-		System.out.println("Substring: " + sub);
-
-		// Trimming space from beginning and end
+		a[0] = 10;
+		b[1] = 20;
 		
-		String trimmed = "    trim me     ".trim();
-		System.out.println(">" + trimmed + "<");
+		print_array("a now", a);
+		print_array("b now", b);
 		
-		// Finding first occurrence of a substring
+		// Similarly, if we send the array to a method 
+		// and that method changes its contents, then
+		// those changes are "seen" via our original
+		// reference
 		
-		int idx = s.indexOf("my");
-		System.out.println(s.substring(idx));
+		print_array("a before add_one_to_all", a);
+		add_one_to_all(a);		
+		print_array("a after add_one_to_all", a);
 		
+		// And since a and b are still both
+		// referencing the same variable, accessing
+		// the array via b gives us the same values
+		
+		print_array("b at this point", b);
+		
+		// The clone() method of arrays will 
+		// create a brand new array of the same
+		// type and copy each element from the
+		// original to the new array and return
+		// the new array. 
+		
+		b = a.clone();
+		
+		// So now, b and a have the same element
+		// values.
+		
+		print_array("a after clone", a);
+		print_array("b after clone", b);
+		
+		// But are not the same array.
+		
+		add_one_to_all(a);
+		
+		print_array("a is modified", a);
+		print_array("b is not", b);
+		
+		// So if you need to pass an array
+		// to a method but want to ensure
+		// that nothing happens to your original
+		// values, pass a clone instead.
+		// Don't even need to create a variable
+		// to do this.
+		
+		print_array("a before passing clone to add_one_to_all", a);
+		add_one_to_all(a.clone());
+		print_array("a after passing clone to add_one_to_all", a);		
 	}
 
+	// This method accepts an array of int's as its second parameter
+	// and prints its contents along with a label provided as its
+	// first parameter.
+	public static void print_array(String label, int[] array_to_print) {
+		System.out.print(label + ": ");
+		for (int i=0; i<array_to_print.length; i++) {
+			System.out.print(array_to_print[i]);
+
+			if (i != array_to_print.length-1) {
+				System.out.print(", ");
+			}
+		}
+		System.out.println();
+	}
+	
+	
+	public static void add_one_to_all(int[] array_to_modify) {
+		for (int i=0; i<array_to_modify.length; i++) {
+			array_to_modify[i] += 1;
+		}
+	}
 }
